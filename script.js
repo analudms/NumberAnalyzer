5c4ac8ed99bfe0f1e322b3af00cef960f139fcff
analudms <analuizamelo3m@gmail.com>
2023-11-13 14:58:40 -0300

    creating function to finish and show results

--- script.js
+++ script.js
@@ -25,5 +25,30 @@ function toAdd(){
     num.value = ""
     num.focus()
 }
+ 
+function finish() {
+    if(values.length == 0){
+        alert('Add values to finish')
+    }else{
+        let total = values.length
+        let max = Math.max.apply(null, values )
+        let min = Math.min.apply(null, values )
+
+        let sum = 0
+        for(let i = 0; i < values.length; i++){
+            sum += values[i]
+        }
+
+        let media = 0
+        media = sum / total
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>In total, we have ${total} numbers registered.</p>`
+        res.innerHTML += `<p>The highest value is ${max} and the lowest value is ${min}.</p>`
+        res.innerHTML += `<p>The total sum is ${sum}</p>`
+        res.innerHTML += `<p>The average is ${media}</p>`
+    }
+}
 
 addButton.addEventListener('click', toAdd)
+secondButton.addEventListener('click', finish)
